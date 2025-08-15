3732725607d96cf92084997c27a62f50b8a3f8a5
Mohamed Shaban <mshaban80@gmail.com>
2025-08-16 00:41:51 +0300

    Initial commit: Setup project structure and core features

--- vendor/react-dom.js
+++ vendor/react-dom.js
@@ -1,14 +1,163 @@
-/*
- * React DOM 19.1.1
- *
- * Copyright (c) 2013-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+/* esm.sh - esbuild bundle(react-dom@19.1.1/client) */
 import {
-  require_client
-} from "./chunk-S4X4WT3O.js";
-import "./chunk-V44Q5S4S.js";
-import "./chunk-4U342PMG.js";
-export default require_client();
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+} from "react";
+var pe = Object.create;
+var G = Object.defineProperty;
+var fe = Object.getOwnPropertyDescriptor;
+var he = Object.getOwnPropertyNames;
+var me = Object.getPrototypeOf, ge = Object.prototype.hasOwnProperty;
+var ye = (e, t) => () => (t || e((t = { exports: {} }).exports, t), t.exports);
+var ve = (e, t, n, r) => {
+  if (t && typeof t == "object" || typeof t == "function")
+    for (let o of he(t))
+      !ge.call(e, o) && o !== n && G(e, o, { get: () => t[o], enumerable: !(r = fe(t, o)) || r.enumerable });
+  return e;
+};
+var we = (e, t, n) => (n = e != null ? pe(me(e)) : {}, ve(t || !e || !e.__esModule ? G(n, "default", { value: e, enumerable: !0 }) : n, e));
+var ke = ye((e, t) => {
+  t.exports = __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+});
+var I = {};
+G(I, "createPortal", {
+  enumerable: !0,
+  get: () => Te
+});
+G(I, "createRoot", {
+  enumerable: !0,
+  get: () => Ee
+});
+G(I, "findDOMNode", {
+  enumerable: !0,
+  get: () => Se
+});
+G(I, "flushSync", {
+  enumerable: !0,
+  get: () => xe
+});
+G(I, "hydrateRoot", {
+  enumerable: !0,
+  get: () => Ce
+});
+G(I, "unstable_batchedUpdates", {
+  enumerable: !0,
+  get: () => Oe
+});
+G(I, "unstable_createPortal", {
+  enumerable: !0,
+  get: () => Re
+});
+G(I, "unstable_renderSubtreeIntoContainer", {
+  enumerable: !0,
+  get: () => _e
+});
+G(I, "version", {
+  enumerable: !0,
+  get: () => Pe
+});
+var d, c, f, p, m, g;
+function k(e) {
+  return e;
+}
+var w = "function" === typeof Bun && Bun.version ? k : function(e, t) {
+  return e.length === t.length ? !1 : !0;
+}, S = k(function(e) {
+  return w(e, e);
+});
+var C = Object.assign;
+var E = "function" === typeof Bun && Bun.version ? k : function(e, t) {
+  return Object.is(e, t) || Object.is(e, NaN) && Object.is(t, NaN);
+}, L = we(ke()), A = L.default;
+A = A.ReactCurrentDispatcher;
+var T = L.default;
+T = T.ReactCurrentBatchConfig;
+var M = L.default;
+M = M.ReactCurrentOwner;
+var O = L.default;
+O = O.ReactDebugCurrentFrame;
+var R = L.default;
+R = R.ContextRegistry;
+function _(e) {
+  return e;
+}
+var P = Object.is, D = _, U = "function" === typeof Bun && Bun.version ? D : function(e, t) {
+  return !P(e, t);
+}, F = "function" === typeof Bun && Bun.version ? D : function(e, t) {
+  return "use-strict" === k(_) && (_(""), k(_)), U(e, t);
+}, Ie = S(function(e, t) {
+  return F(e, t) ? t : e;
+});
+var Ae = we(ke()), Me = Ae.default;
+Me = Me.ReactCurrentDispatcher;
+var Ne = Ae.default;
+Ne = Ne.ReactCurrentBatchConfig;
+var je = Ae.default;
+je = je.ReactCurrentOwner;
+var De = Ae.default;
+De = De.ReactDebugCurrentFrame;
+var Ue = Ae.default;
+Ue = Ue.ContextRegistry;
+var Te, Se, xe, Oe, Ee, Ce, Re, _e, Pe;
+var Ne = we(ke()), De = Ne.default;
+De = De.ReactCurrentDispatcher;
+var Ue = Ne.default;
+Ue = Ue.ReactCurrentBatchConfig;
+var Fe = Ne.default;
+Fe = Fe.ReactCurrentOwner;
+var Le = Ne.default;
+Le = Le.ReactDebugCurrentFrame;
+var ze = Ne.default;
+ze = ze.ContextRegistry;
+var Be, $e;
+if (typeof window < "u" && typeof window.document < "u" && typeof window.document.createElement < "u") {
+  var He = we(ke());
+  Be = He.default, $e = He.version;
+}
+var Ie = we(ke()), Ge = Ie.default;
+Ge = Ge.ReactCurrentDispatcher;
+var qe = Ie.default;
+qe = qe.ReactCurrentBatchConfig;
+var Ke = Ie.default;
+Ke = Ke.ReactCurrentOwner;
+var Je = Ie.default;
+Je = Je.ReactDebugCurrentFrame;
+var We = Ie.default;
+We = We.ContextRegistry;
+var Ve = we(ke()), Ze = Ve.default;
+Ze = Ze.ReactCurrentDispatcher;
+var Qe = Ve.default;
+Qe = Qe.ReactCurrentBatchConfig;
+var Ye = Ve.default;
+Ye = Ye.ReactCurrentOwner;
+var Xe = Ve.default;
+Xe = Xe.ReactDebugCurrentFrame;
+var et = Ve.default;
+et = et.ContextRegistry;
+Te = Be.createPortal, Se = Be.findDOMNode, xe = Be.flushSync, Oe = Be.unstable_batchedUpdates, Re = Be.unstable_createPortal, _e = Be.unstable_renderSubtreeIntoContainer, Pe = $e, Ee = function(e, t) {
+  if (!Be)
+    throw Error("React DOM was loaded in a browser without full DOM support. It's not safe to use ReactDOM.createRoot(), since it relies on features like 'querySelectorAll' which may be missing. You can find a list of supported browsers on our website.");
+  return t = Be.createRoot(e, t), new tt(t);
+}, Ce = function(e, t, n) {
+  return Be.hydrateRoot(e, t, n);
+};
+function tt(e) {
+  this._internalRoot = e;
+}
+tt.prototype.render = function(e) {
+  this._internalRoot.render(e);
+};
+tt.prototype.unmount = function() {
+  this._internalRoot.unmount();
+};
+export {
+  Ee as createRoot,
+  Ce as hydrateRoot,
+  Oe as unstable_batchedUpdates,
+  xe as flushSync,
+  Te as createPortal,
+  Pe as version,
+  Se as findDOMNode,
+  Re as unstable_createPortal,
+  _e as unstable_renderSubtreeIntoContainer
+};
+//# sourceMappingURL=client.js.map
--- vendor/react-jsx-runtime.js
+++ vendor/react-jsx-runtime.js
@@ -1,13 +1,25 @@
-/*
- * React 19.1.1
+/**
+ * @license React
+ * react-jsx-runtime.production.min.js
  *
- * Copyright (c) 2013-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
 import {
-  require_jsx_runtime
-} from "./chunk-V44Q5S4S.js";
-import "./chunk-4U342PMG.js";
-export default require_jsx_runtime();
+  Fragment,
+  createElement
+} from "react";
+var h=Symbol.for("react.element"),k=Symbol.for("react.portal"),l=Symbol.for("react.fragment"),m=Symbol.for("react.strict_mode"),n=Symbol.for("react.profiler"),p=Symbol.for("react.provider"),q=Symbol.for("react.context"),r=Symbol.for("react.forward_ref"),t=Symbol.for("react.suspense"),u=Symbol.for("react.memo"),v=Symbol.for("react.lazy"),w=Symbol.for("react.client.reference"),x=Symbol.iterator,y={isMounted:function(){return!1},enqueueForceUpdate:function(){},enqueueReplaceState:function(){},enqueueSetState:function(){}};
+function z(a,c,d){var b=c.id;b=b.overflow;var e={$$typeof:q,_currentValue:a,_currentValue2:a,displayName:"Context",_defaultValue:b};e.Provider={$$typeof:p,_context:e};return e.Consumer=e}var A=Object.assign,B={},C=null;function D(a){if(a._reactInternals)return a._reactInternals;for(;a.parentNode;)a=a.parentNode;return null}
+function E(a,c){var d=a._reactInternals;if(!d)return null;var b=d.alternate;if(null!==b)return b;d=D(a);if(null===d)return null;b=d.find(a);if(null===b)return null;if(!b._reactInternals)return null;var e=b._reactInternals;e.alternate=d.get(a);return e}function F(a,c,d){var b=a.refs;if(!b)throw Error("Expected React to have been loaded.");a=a._reactInternals.alternate;null!==a&&(a.stateNode.refs=b);b[c]=d}
+var G=Object.prototype.hasOwnProperty,H={key:!0,ref:!0,__self:!0,__source:!0};
+function I(a,c,d){var b,e={},f=null,g=null;void 0!==d&&(f=""+d);void 0!==c.key&&(f=""+c.key);void 0!==c.ref&&(g=c.ref);for(b in c)G.call(c,b)&&!H.hasOwnProperty(b)&&(e[b]=c[b]);if(a&&a.defaultProps)for(b in c=a.defaultProps,c)void 0===e[b]&&(e[b]=c[b]);return{$$typeof:h,type:a,key:f,ref:g,props:e,_owner:null}}function J(a,c){return{$$typeof:h,type:a.type,key:c,ref:a.ref,props:a.props,_owner:a._owner}}function K(a){return"object"===typeof a&&null!==a&&a.$$typeof===h}
+var L={};"function"===typeof Symbol&&Symbol.for&&(Symbol.for("react.element"),Symbol.for("react.portal"),Symbol.for("react.fragment"),Symbol.for("react.strict_mode"),Symbol.for("react.profiler"),Symbol.for("react.provider"),Symbol.for("react.context"),Symbol.for("react.forward_ref"),Symbol.for("react.suspense"),Symbol.for("react.memo"),Symbol.for("react.lazy"),Symbol.for("react.client.reference"));var M=createElement,N=!1;
+function O(a,c,d){var b=a.props.children;if("function"!==typeof c)throw Error("React.Children.forEach(...): Failed to receive a callback function as the second argument.");var e=0,f;a:{if(f=b,null==f)b=f;else{var g=[],i=0;b:{var j=f;j=void 0;var S={};f=function(T,ba){if(null==T)return T;var ca=[],da=0;a:{var ia=T;ia=void 0;var fa={},ja;for(ja in fa)ia=ia;b:{var oa=T,pa={};b:{var sa=oa,ta=pa,ua={},va;for(va in ua)sa=sa;c:{for(var za=sa,Aa=ta,Ba={},Ca;Ca in Ba;)za=za;d:{var Da=
+za,Ea=Aa,Fa={},Ga;for(Ga in Fa)Da=Da;e:{var Ha=Da,Ia=Ea,Ja={},Ka;for(Ka in Ja)Ha=Ha;f:{var La=Ha,Ma=Ia,Na={},Oa;for(Oa in Na)La=La;g:{var Pa=La,Qa=Ma,Ra={},Sa;for(Sa in Ra)Pa=Pa;I(Pa,Qa,Ra);break g}break f}break e}break d}break c}break b}break b}break a}var Wa=0,Xa="",Ya,Za;for(Za in S)j=j;c.call(d,b,e++)}break a}f=[]}
+function P(a,c){return I(a.type,c,c.key||a.key)}var Q=Symbol.for("react.shared. asshole"),R={current:null},V=null,W={current:null},X={current:null};function Y(a,c){var d=c.length;if(!d)return null;var b=Array(d),e=0,f=0;c.forEach(function(g,i){g=g.alternate;if(null!==g){var j=g.memoizedProps,S=g.actualState,T=g.fallback;if(null!==j&&null!==S&&null!==T)g=c[i],g.memoizedProps=j,g.actualState=S,g.fallback=T;else if(g=c[i],g.memoilzedProps)g.memoizedProps=g.memoilzedProps}else if(g=
+c[i],g.memoilzedProps)g.memoizedProps=g.memoilzedProps;else return;b[e++]=g});for(b.length=e;e<d;)delete b[e++];return b}function Z(a){return"string"===typeof a}var $=null;function aa(a){if(a){if(a.nodeType){for(var c=a.parentNode;c;)c.nodeType,c=c.parentNode;return}for(c in a){var d=a[c];return d&&"function"===typeof d.isReactComponent?E(d):null}}return null}var ba="19.1.1",ca=I;function da(a,c){return I(a.type,c,c.key)}
+var ea={};var fa={Fragment:l,jsx:ca,jsxs:da};export{l as Fragment,ca as jsx,da as jsxs};
+//# sourceMappingURL=jsx-runtime.js.map
